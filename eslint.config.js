7a90cd03145cb6ffeb5463cf14494241e2e14d13
SamuelNakab <samuelnakab@gmail.com>
2026-06-28 23:21:29 -0300

    Configurar pipeline de CI/CD: lint, tests unitarios, E2E y deploy

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        Buffer: 'readonly',
+        URL: 'readonly',
+        fetch: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error'
+    }
+  }
+];
